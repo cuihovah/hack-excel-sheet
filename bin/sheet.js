0366969a7e40c792227fd77235f67f29f2db17f8
cuihovah <cuihovah@gmail.com>
2017-08-07 21:19:34 +0800

    update eslint

--- bin/sheet.js
+++ bin/sheet.js
@@ -14,51 +14,51 @@ const Thenjs = require('thenjs');
  */
 
 program
-	.usage('<command> filename')
-	.parse(process.argv);
+    .usage('<command> filename')
+    .parse(process.argv);
 
 var args = program.args;
 
 function help () {
-	if (program.args.length < 1) 
-		return program.help()
+    if (program.args.length < 1) 
+        return program.help();
 }
 
-help()
+help();
 
 function process(done, file) {
-	var readStream = fs.createReadStream(file);
-	var str = '';
-	readStream.on('data', function(buf){
-		str += buf.toString();		
-	});
+    var readStream = fs.createReadStream(file);
+    var str = '';
+    readStream.on('data', function(buf){
+        str += buf.toString();		
+    });
 
-	readStream.on('end', function(){
-		str = str.replace(/<sheet[^>]>/g, '');
-		fs.write(file, str, function(err){
-			done();	
-		});		
-	});
+    readStream.on('end', function(){
+        str = str.replace(/<sheet[^>]>/g, '');
+        fs.write(file, str, function(err){
+            done();	
+        });		
+    });
 }
 
 function run() {
-	var filename = program.args[0];
+    var filename = program.args[0];
 
-	decompress(filename, 'dist', {
-		plugins: [
-			decompressUnzip()
-		]
-	}).then(() => {
-		Thenjs.each([], function(cont, value){
-			process(value, cont);	
-		}).then(function(cont, result){
-			zipdir(path.join('dist'), { 
-				saveTo: './myzip.xlsx' 
-			}, function (err, buffer) {
-				console.log('123');
-			});
-		});
-	});
+    decompress(filename, 'dist', {
+        plugins: [
+            decompressUnzip()
+        ]
+    }).then(() => {
+        Thenjs.each([], function(cont, value){
+            process(value, cont);	
+        }).then(function(cont, result){
+            zipdir(path.join('dist'), { 
+                saveTo: './myzip.xlsx' 
+            }, function (err, buffer) {
+                console.log('123');
+            });
+        });
+    });
 }
 
 run();
